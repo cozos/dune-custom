a3c3be735da699b7c60f7217edecd872f1059981
Arwin Tio <arwin.tio@getcruise.com>
2025-04-05 04:27:39 -0700

    Fix problem

--- holden.utf8.lua
+++ holden.utf8.lua
@@ -19455,13 +19455,14 @@ local multiply = CardEffect.multiply
                                                                                                             local card = Helper.getCard(ImperiumRow.slotZones[5])
                                                                                                             local continuation = Helper.createContinuation("ImperiumRow.wormEatsTheCard")
                                                                                                             if card then
-                                                                                                                card.addForce(Vector(math.random(10, 30), math.random(1000, 2000), math.random(10, 30)))
-                                                                                                                card.addTorque(Vector(math.random(1000, 2000), math.random(1000, 2000), math.random(1000, 2000)))
+                                                                                                                card.addForce(Vector(math.random(-500, 500), math.random(2000, 4000), math.random(-500, 500)))
+                                                                                                                card.addTorque(Vector(math.random(-5000, 5000), math.random(-5000, 5000), math.random(-5000, 5000)))
 
                                                                                                                 Wait.time(function()
-                                                                                                                    MainBoard.trash(card)
+                                                                                                                    -- MainBoard.trash(card)
+                                                                                                                    card.destruct()
                                                                                                                     continuation.run()
-                                                                                                                end, 4)
+                                                                                                                end, 0.5)
                                                                                                             else
                                                                                                                 continuation.run()
                                                                                                             end
