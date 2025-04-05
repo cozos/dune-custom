51e4e142479ff794927e77c4b413539b7ff7e91c
Arwin Tio <arwin.tio@getcruise.com>
2025-04-05 04:42:13 -0700

    Yeet

--- holden.utf8.lua
+++ holden.utf8.lua
@@ -19455,14 +19455,14 @@ local multiply = CardEffect.multiply
                                                                                                             local card = Helper.getCard(ImperiumRow.slotZones[5])
                                                                                                             local continuation = Helper.createContinuation("ImperiumRow.wormEatsTheCard")
                                                                                                             if card then
-                                                                                                                card.addForce(Vector(math.random(-100, 100), math.random(2000, 2500), math.random(-400, 400)))
-                                                                                                                card.addTorque(Vector(math.random(-1000, 1000), math.random(-1000, 1000), math.random(-1000, 1000)))
+                                                                                                                card.addForce(Vector(math.random(-20, 20), math.random(800, 21500), math.random(30, 80)))
+                                                                                                                card.addTorque(Vector(math.random(-2000, 2000), math.random(-2000, 2000), math.random(-2000, 2000)))
 
                                                                                                                 Wait.time(function()
                                                                                                                     -- MainBoard.trash(card)
                                                                                                                     card.destruct()
                                                                                                                     continuation.run()
-                                                                                                                end, 1.5)
+                                                                                                                end, 1.75)
                                                                                                             else
                                                                                                                 continuation.run()
                                                                                                             end
