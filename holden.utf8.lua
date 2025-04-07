3f3c9111ea37bb326e9239e500706a9bda90292f
Arwin Tio <arwin.tio@getcruise.com>
2025-04-06 23:48:50 -0700

    Yeet

--- holden.utf8.lua
+++ holden.utf8.lua
@@ -19477,7 +19477,7 @@ local multiply = CardEffect.multiply
                                                                                                         end
 
                                                                                                         function ImperiumRow.yeet()
-                                                                                                            log("ARWIN: yeet!")
+                                                                                                            log(string.format("ARWIN: yeeeet! %s", ImperiumRow.deckZone))
                                                                                                             local cards = Helper.getCards(Helper.getDeckOrCard(ImperiumRow.deckZone))
                                                                                                             for _, card in ipairs(cards) do
                                                                                                                 card.addForce(Vector(math.random(-20, 20), math.random(300, 500), 40))
